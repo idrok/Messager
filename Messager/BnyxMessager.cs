fc57e67d624d0a7182968a8ae88b06f52eb94570
leng <11413947@qq.com>
2020-04-29 17:24:57 +0800

    async

--- Messager/BnyxMessager.cs
+++ Messager/BnyxMessager.cs
@@ -79,7 +79,8 @@ namespace Bnyx.Messager {
             else
             {
                 //async TODO
-                
+                // async wrap sync code ???
+                Observable.Start(() => { }, Scheduler.ThreadPool);
             }
         }
 
