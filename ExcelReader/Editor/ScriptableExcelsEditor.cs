36f99fd8def4f79016c5cfb2cdd19a7f022d8eb0
leng <11413947@qq.com>
2020-05-18 15:59:22 +0800

    excel annvoation buildin tested

---
+++ ExcelReader/Editor/ScriptableExcelsEditor.cs
@@ -0,0 +1,65 @@
+﻿using System.IO;
+using Tang.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace GameInnvoation.ExcelReader
+{
+    [CustomEditor(typeof(ScriptableExcels))]
+    public class ScriptableExcelsEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            
+            GUILayout.Space(10);
+            var btnVaildPath = GUILayout.Button("Step1.验证xlsx资源的路径", GUILayout.Height(30));
+            GUILayout.Space(5);
+            var btnImport =    GUILayout.Button("Step2.导入xlsx数据到资源", GUILayout.Height(30));
+            GUILayout.Space(5);
+            var btnDataVaild = GUILayout.Button("Step3.校验xlsx数据的完整", GUILayout.Height(30));
+
+            if (btnVaildPath)
+            {
+                CheckResPath();
+            }
+            
+            if (btnImport)
+            {
+                var scriptable = (ScriptableExcels) target;
+                Debug.LogFormat(scriptable._IsRelativePath + "");
+            }
+        }
+
+        private void CheckResPath() 
+        {
+             var scriptable = (ScriptableExcels)target;
+             var origin = scriptable._ExcelPath;
+             var paths = origin.Split(',');
+
+             foreach (var path in paths)
+             {
+                 if (scriptable._IsRelativePath == false)
+                 {
+                     var vaild = File.Exists(path);
+                     if (vaild == false)
+                     {
+                         EditorUtility.DisplayDialog("文件路径校验", $"{path}\r\n路径无效？", "确定");
+                     }
+                 }
+                 
+                 if (scriptable._IsRelativePath == true)
+                 {
+                     var filePath = Path.Combine(Application.dataPath, path);
+                     var valid = File.Exists(filePath);
+                     if (valid == false)
+                     {
+                         EditorUtility.DisplayDialog("文件路径校验", $"{path}\r\n路径无效？", "确定");
+                     }
+                 }
+             }
+             //File.Exists()
+
+        }
+    }
+}
---
+++ ExcelReader/ScriptableExcels.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+namespace GameInnvoation.ExcelReader
+{
+    /// <summary>
+    /// 该类为保存数据的持久化
+    /// </summary>
+    [System.Serializable]
+    [CreateAssetMenu(fileName = "innvoation", menuName = "ExcelReader", order = 0)]
+    public class ScriptableExcels : ScriptableObject
+    {
+        // 相对文件路径，相对于Application.dataPath
+        // 在编辑器中的项目的相对跟路径即就是Assets目录里那一层
+        // C:/work/bnyx/Assets
+        [Header("是否是相对文件路径")]
+        public bool _IsRelativePath;
+        
+        [Multiline, Header("xlsx文件路径，多个文件逗号(,)分隔")]
+        public string _ExcelPath;
+
+        private byte[] _ExcelMemoryData;
+
+        public byte[] ExcelMemoryData
+        {
+            get => _ExcelMemoryData;
+            set => _ExcelMemoryData = value;
+        }
+
+        private void OnEnable()
+        {
+            Debug.LogFormat($"OnEnable");
+        }
+
+        private void OnDisable()
+        {
+            Debug.LogFormat($"OnDisable");
+        }
+
+        private void OnDestroy()
+        {
+            Debug.LogFormat($"OnDestroy");
+        }
+    }
+}
